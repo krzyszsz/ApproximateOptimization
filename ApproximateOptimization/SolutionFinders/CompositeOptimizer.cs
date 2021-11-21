ff1951b0c8c2a3c1227119be1046178557b3d754
Krzysztof Janiszewski <krzysztofjaniszewski@users.noreply.github.com>
2021-11-21 22:10:07 +0000

    Moved to dotnet 6.0 and added examples.

--- ApproximateOptimization/SolutionFinders/CompositeOptimizer.cs
+++ ApproximateOptimization/SolutionFinders/CompositeOptimizer.cs
@@ -7,10 +7,6 @@ namespace ApproximateOptimization
     /// </summary>
     public class CompositeOptimizer : MultithreadedOptimizer
     {
-        private ILogger logger;
-        private int threadCount;
-        private Func<int, ISolutionFinder> createSolutionFinder;
-
         public CompositeOptimizer(int threadCount = 8, ILogger logger = null, double temperatureMultiplier = 0.9, int randomSeed = 0,
             double localAreaMultiplier = 0.2, int iterationCount = 3, int iterationsPerDimension = 10)
             : base(threadId => new SimulatedAnnealingWithLocalAreaBinarySearch(
