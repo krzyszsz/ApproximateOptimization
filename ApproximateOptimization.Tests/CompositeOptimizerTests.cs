4d565d779708d978fa306675dde5ae1cc1103ee8
Krzysztof Janiszewski <krzysztofjaniszewski@users.noreply.github.com>
2021-11-21 18:39:16 +0000

    Composite Optimizer.

---
+++ ApproximateOptimization.Tests/CompositeOptimizerTests.cs
@@ -0,0 +1,60 @@
+using NUnit.Framework;
+using System;
+
+namespace ApproximateOptimization.Tests
+{
+    public class CompositeOptmizerTests
+    {
+        private CompositeOptimizer GetSut()
+        {
+            return new CompositeOptimizer();
+        }
+
+        [Test]
+        public void FindsGoodSolutionForLinearFunctionGrowingInBothDimentions()
+        {
+            Func<double[], double> func = (double[] vector) => vector[0] + vector[1];
+            var sut = GetSut();
+
+            sut.FindMaximum(2, func, default(TimeSpan), 100);
+
+            Assert.That(sut.BestSolutionSoFar.Length, Is.EqualTo(2));
+            Assert.That(sut.BestSolutionSoFar[0], Is.EqualTo(1).Within(0.01));
+            Assert.That(sut.BestSolutionSoFar[1], Is.EqualTo(1).Within(0.01));
+            Assert.That(sut.SolutionValue, Is.EqualTo(2).Within(0.01));
+        }
+
+        [Test]
+        public void FindsGoodSolutionForLinearFuncDecreasingInBothDimensions()
+        {
+            Func<double[], double> func = (double[] vector) => -vector[0] - vector[1];
+            var sut = GetSut();
+
+            sut.FindMaximum(2, func, default(TimeSpan), 100);
+
+            Assert.That(sut.BestSolutionSoFar.Length, Is.EqualTo(2));
+            Assert.That(sut.BestSolutionSoFar[0], Is.EqualTo(0).Within(0.01));
+            Assert.That(sut.BestSolutionSoFar[1], Is.EqualTo(0).Within(0.01));
+            Assert.That(sut.SolutionValue, Is.EqualTo(0).Within(0.01));
+        }
+
+        [Test]
+        public void FindsGoodSolutionForSinCos()
+        {
+            Func<double[], double> func = (double[] vector) =>
+                Math.Sin(vector[0] * (2 * Math.PI)) + Math.Cos((vector[1] - 0.4) * (2 * Math.PI));
+            var sut = GetSut();
+            double expectedX = 0.25;
+            double expectedY = 0.4;
+            double expectedBestValue = 2;
+
+            sut.FindMaximum(2, func, default(TimeSpan), 1000);
+
+            Assert.That(sut.BestSolutionSoFar.Length, Is.EqualTo(2));
+            Assert.That(sut.SolutionFound, Is.EqualTo(true));
+            Assert.That(sut.BestSolutionSoFar[0], Is.EqualTo(expectedX).Within(0.01));
+            Assert.That(sut.BestSolutionSoFar[1], Is.EqualTo(expectedY).Within(0.01));
+            Assert.That(sut.SolutionValue, Is.EqualTo(expectedBestValue).Within(0.01));
+        }
+    }
+}
---
+++ ApproximateOptimization/SolutionFinders/CompositeOptimizer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ApproximateOptimization
+{
+    /// <summary>
+    /// This optimizer employs MultithreadedOptimizer to run SimulatedAnnealingWithLocalAreaBinarySearch.
+    /// </summary>
+    public class CompositeOptimizer : MultithreadedOptimizer
+    {
+        private ILogger logger;
+        private int threadCount;
+        private Func<int, ISolutionFinder> createSolutionFinder;
+
+        public CompositeOptimizer(int threadCount = 8, ILogger logger = null, double temperatureMultiplier = 0.9, int randomSeed = 0,
+            double localAreaMultiplier = 0.2, int iterationCount = 3, int iterationsPerDimension = 10)
+            : base(threadId => new SimulatedAnnealingWithLocalAreaBinarySearch(
+                    temperatureMultiplier,
+                    threadId,
+                    localAreaMultiplier,
+                    iterationCount,
+                    iterationsPerDimension),
+                 threadCount,
+                 logger)
+        {
+        }
+    }
+}
