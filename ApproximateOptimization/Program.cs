3b5d672cdf4a6925b2f7c9572e4a8d4c1526425d
Krzysztof Janiszewski <krzysztofdotandrzejdotjaniszewskiat g m a i ldotc o m>
2021-11-21 22:10:07 +0000

    Moved to dotnet 6.0 and added examples.

--- ApproximateOptimization/Program.cs
+++ ApproximateOptimization/Program.cs
@@ -1,12 +1,76 @@
 ﻿using System;
+using System.Linq;
 
 namespace ApproximateOptimization
 {
     class Program
     {
+        private static void Example1_FindMaximum()
+        {
+            // Finds maximum of sin(x) * cos(y) for range x: 0..1 and y: 0..1 (there are easy solutions without the solver, this is just an example)
+            var func = (double[] vector) => Math.Sin(vector[0]) * Math.Cos(vector[1]);
+            var optimizer = new CompositeOptimizer();
+            optimizer.FindMaximum(2, func, maxIterations: 100); // 2 means that we have 2 dimensions (i.e. 2 numbers in vector in the function)
+            Console.WriteLine($"Example1: maximum value {optimizer.SolutionValue} was found for x={optimizer.BestSolutionSoFar[0]} and y={optimizer.BestSolutionSoFar[1]} for x&y in range 0..1.");
+            // This prints:
+            // Example1: maximum value 0.8414709848078965 was found for x=1 and y=0 for x&y in range 0..1.
+        }
+
+        struct Point { public double x; public double y; }
+
+        private static void Example2_Linear_regression()
+        {
+            // Finds simple regression line for a couple of points. To do that we need to minimize the sum of vertical distances to our line (there are faster ways than using a solver, this is just an example).
+            // This obviously can use more complex regression models, not only lines (imagine: trygonometric functions if we wnated to make a slow version of discrete frequency transformation).
+            var points = new Point[] {
+                new Point{ x = 13.2, y = 2.3 },
+                new Point{ x = 23.3, y = 3.4 },
+                new Point{ x = 33.6, y = 4.1 },
+                new Point{ x = 34.7, y = 5.3 },
+            };
+            var regressionLine = (double[] coefficients, Point point) => (coefficients[0] * point.x) + coefficients[1];
+            var errorFunction = (double[] coefficients) => points.Sum(point => Math.Abs(regressionLine(coefficients, point) - point.y));
+            var reverseErrorFunction = (double[] coefficients) => 1 / (errorFunction(coefficients) + 1);    // We need to reverse the function to maximize it rather than minimize it.
+            var optimizer = new CompositeOptimizer();
+            optimizer.FindMaximum(2, reverseErrorFunction, maxIterations: 100); // 2 means that we have 2 dimensions (i.e. 2 numbers in vector in the function)
+            Console.WriteLine($"Example2: Found regression line y = {optimizer.BestSolutionSoFar[0] :N4}*x + {optimizer.BestSolutionSoFar[1] :N4}");
+            // This prints:
+            // Example2: Found regression line y = 0.1089*x + 0.8624
+        }
+
+        private static void Example3_Linear_regression_with_result_range_rescaled()
+        {
+            // This example is similar to example 2, but we want the results to be possibly in a bigger range, not only in 0..1.
+            // To do that we need to scale the result. In this example we scale the results so that search area for solutions is between -1 million and +1 million.
+            var scaleFactor = 1000000;
+            var rescale = (double inputNumber) => inputNumber * scaleFactor * 2 - scaleFactor;
+
+            var points = new Point[] {
+                new Point{ x = 0.2, y = 33.6 },
+                new Point{ x = 0.3, y = 24.7 },
+                new Point{ x = 0.6, y = -71.8 },
+                new Point{ x = 0.7, y = -73.9 },
+            };
+            var regressionLine = (double[] coefficients, Point point) => (rescale(coefficients[0]) * point.x) + rescale(coefficients[1]);
+            var errorFunction = (double[] coefficients) => points.Sum(point => Math.Abs(regressionLine(coefficients, point) - point.y));
+            var reverseErrorFunction = (double[] coefficients) => 1 / (errorFunction(coefficients) + 1);    // We need to reverse the function to maximize it rather than minimize it.
+            var optimizer = new CompositeOptimizer();
+            optimizer.FindMaximum(2, reverseErrorFunction, maxIterations: 100); // 2 means that we have 2 dimensions (i.e. 2 numbers in vector in the function)
+            Console.WriteLine($"Example3: Found regression line y = {rescale(optimizer.BestSolutionSoFar[0]) :N4}*x + {rescale(optimizer.BestSolutionSoFar[1]) :N4}");
+            // This prints:
+            // Example3: Found regression line y= -218.4419*x + 78.9524
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Example1_FindMaximum();
+
+            Example2_Linear_regression();
+
+            Example3_Linear_regression_with_result_range_rescaled();
+
+            Console.WriteLine("Press Enter.");
+            Console.ReadLine();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # ApproximateOptimization
-Heuristic optimizer using simulated annealing for problems whose solution is a vector of n-numbers in range <0, 1> and where we have a function returning a score (value) for each solution (the higher, the better). https://en.wikipedia.org/wiki/Simulated_annealing
+Heuristic optimizer using simulated annealing for problems whose solution is a vector of n-numbers in range <0, 1> and where we have a function returning a score (value) for each solution (the higher, the better). See [Wikipedia: Simulated annealing](https://en.wikipedia.org/wiki/Simulated_annealing)
 
-Obviously a lot of problems can be adapted to this optimizer.
+On top of that this package contains "LocalAreaBinarySearch" (running for each problem dimension independently) working well only for certain types of problems and for small local areas of the function. Both these optimizers are composed into third type: "SimulatedAnnealingWithLocalAreaBinarySearch" which uses simulated annealing every second iteration and checks local neighberhood with binary search.
+The final type "CompositeOptimizer" is additionally running the optimizers in multiple threads and returning the best result.
 
 I'm aware there are a lot of other optimizers / solvers available but this one aims to be very simple and therefore easy to re-adjust to individual needs.
 
-** This is still work in progress: ** I want to add a composite optimizer that combines simulated annealing with local area binary search and runs it in multiple threads.
+Example:
 
-
-TODO: Example
