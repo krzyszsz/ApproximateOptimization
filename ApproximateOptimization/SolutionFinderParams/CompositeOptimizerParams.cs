5fa230d31de5c3f0d7810cfbb4ff30eb3535214b
Krzysztof Janiszewski <krzysztofjaniszewski@outlook.com>
2021-11-28 14:23:50 +0000

    wip

---
+++ ApproximateOptimization/SolutionFinderParams/CompositeOptimizerParams.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ApproximateOptimization
+{
+    public class CompositeOptimizerParams<T> : MultiThreadedOptimizerParams<T> where T : BaseSolutionFinderParams, new()
+    {
+        public T ActualOptimizerParams { get; set; } = new T();
+
+        public override void Validate()
+        {
+            base.Validate();
+            if (ActualOptimizerParams != null)
+            {
+                ActualOptimizerParams.Validate();
+            }
+            else
+            {
+                throw new ArgumentException("Missing argument ActualOptimizerParams");
+            }
+        }
+    }
+}
--- ApproximateOptimization/SolutionFinders/CompositeOptimizer.cs
+++ ApproximateOptimization/SolutionFinders/CompositeOptimizer.cs
@@ -1,25 +1,16 @@
-﻿using System;
-
-namespace ApproximateOptimization
+﻿namespace ApproximateOptimization
 {
     /// <summary>
     /// This optimizer employs MultithreadedOptimizer to run SimulatedAnnealingWithLocalAreaBinarySearch.
     /// </summary>
-    public class CompositeOptimizer : MultithreadedOptimizer<SimulatedAnnealingWithLocalAreaBinarySearchParams>
+    public class CompositeOptimizer<T, P> : MultithreadedOptimizer<T>
+        where T : MultiThreadedOptimizerParams<P>, new()
+        where P : BaseSolutionFinderParams
     {
-        public CompositeOptimizer(int threadCount = 8, ILogger logger = null, double temperatureMultiplier = 0.99,
-            double localAreaMultiplier = 0.2, int iterationCount = 3, int iterationsPerDimension = 10)
-            : base(threadId => new SimulatedAnnealingWithLocalAreaBinarySearch(
-                    temperatureMultiplier,
-                    threadId,
-                    false,
-                    true,
-                    localAreaMultiplier,
-                    iterationCount,
-                    iterationsPerDimension),
-                 threadCount,
-                 logger)
+        public CompositeOptimizer(CompositeOptimizerParams<T> problemParams)
+            : base(problemParams)
         {
+            var q = (MultiThreadedOptimizerParams<T>)problemParams;
         }
     }
 }
