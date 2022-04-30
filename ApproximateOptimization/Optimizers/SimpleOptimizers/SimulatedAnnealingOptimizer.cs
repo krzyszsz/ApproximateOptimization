989ab03219fb490a6a721e19abe2f5e888e21f4d
krzysztofjaniszewski <krzysztofjaniszewski@users.noreply.github.com>
2022-04-30 23:03:04 +0100

    Clean up

--- ApproximateOptimization/Optimizers/SimpleOptimizers/SimulatedAnnealingOptimizer.cs
+++ ApproximateOptimization/Optimizers/SimpleOptimizers/SimulatedAnnealingOptimizer.cs
@@ -48,23 +48,6 @@ namespace ApproximateOptimization
                 }
             }
 
-
-            //for (int i=0; i< _problemParameters.Dimension; i++)
-            //{
-            //    var rangeWidth = _problemParameters.SolutionRange[i][1] - _problemParameters.SolutionRange[i][0];
-            //    _currentSolution[i] = BestSolutionSoFar[i] + (_random.NextDouble() * 2.0 * rangeWidth - rangeWidth) * _temperature;
-            //    if (_currentSolution[i] > _problemParameters.SolutionRange[i][1])
-            //    {
-            //        var excess = _currentSolution[i] - _problemParameters.SolutionRange[i][0];
-            //        var excessInRange = excess - Math.Floor(excess / rangeWidth) * rangeWidth;
-            //        _currentSolution[i] = _problemParameters.SolutionRange[i][0] + excessInRange;
-            //    } else if (_currentSolution[i] < _problemParameters.SolutionRange[i][0])
-            //    {
-            //        var deficit = _problemParameters.SolutionRange[i][1] - _currentSolution[i];
-            //        var deficitInRange = deficit - Math.Floor(deficit / rangeWidth) * rangeWidth;
-            //        _currentSolution[i] = _problemParameters.SolutionRange[i][1] - deficitInRange;
-            //    }
-            //}
             _temperature *= _problemParameters.TemperatureMultiplier;
             LocalAreaAtTheEnd = _temperature;
             return GetCurrentValueAndUpdateBest();
